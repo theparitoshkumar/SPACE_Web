961f24690e266619819a7176e2dd9f44fe406d16
theparitoshkumar <theparitoshkumar@gmail.com>
2025-05-22 22:39:10 +0530

    Including the GUI for the brwoser, it is version 1.0

--- browser_py/browser.py
+++ browser_py/browser.py
@@ -1,5 +1,7 @@
 import socket
 import ssl
+import tkinter
+
 
 class URL:
     def __init__(self, url):
@@ -47,7 +49,9 @@ class URL:
         return content
 
 
-def show(body):
+def lex(body):
+    ''' prev: show(body) => lex(body )'''
+    text = ""
     in_tag = False
     for c in body:
         if c == "<":
@@ -55,15 +59,64 @@ def show(body):
         elif c == ">":
             in_tag = False
         elif not in_tag:
-            print(c, end="")
+            text += c
+    return text
 
 
-def load(url):
-    body = url.request()
-    show(body)
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+cursor_x, cursor_y = HSTEP, VSTEP
 
+
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+        if cursor_x > WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+    return display_list
+
+SCROLL_STEP = 100
+
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
+        self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown) 
+        self.window.bind("<Up>", self.scrollup)
+    
+    def draw(self):
+        self.canvas.delete("all")
+        for x,y, c in self.display_list:
+            if y > self.scroll + HEIGHT:
+                continue
+            if y + VSTEP < self.scroll:
+                continue
+            self.canvas.create_text(x, y - self.scroll, text=c)
+
+    def load(self, url):
+        body = url.request()
+        text = lex(body)
+        self.display_list = layout(text)
+        self.draw()
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+    def scrollup(self, e):
+        self.scroll -= SCROLL_STEP
+        self.draw()
 
 if __name__ == "__main__":
     import sys
-    load(URL(sys.argv[1]))
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
+    
 
